7f09eae62236efe51a4f27680db9c2357380aa76
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-11-25 10:57:49 -0500

    display people in html page

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,9 +1,11 @@
 (ns people-clojure.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn -main [& args]
+(defn read-people []                                        ;moved all the code below from the main function into the read-people function
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line]
@@ -21,5 +23,28 @@
         people (filter (fn [line]
                          (= "Brazil" (:country line)))
                        people)]
-    (spit "filtered_people.edn"
-          (pr-str people))))
+    #_(spit "filtered_people.edn"
+          (pr-str people))
+    people))
+
+(defn people-html []
+  (let [people (read-people)]
+    (map (fn [line]
+           [:p                          ;this could also be ":br"
+            (str (:first_name line)     ;str used to concatenate two things together
+                 " "                    ;space is so it shows firstname SPACE lastname on the web server
+                 (:last_name line))])
+         people)))
+
+(defn handler [request]
+  {:status  200
+   :headers {"Content-type" "text/html"}
+   :body    (h/html [:html
+                     [:body
+                      [:a {:href "http://www.theironyard.com"}
+                       "The Iron Yard"]
+                      [:br]
+                      (people-html)]])})
+
+(defn -main [& args]
+  (j/run-jetty #'handler {:port 3000 :join? false}))                       ;port is the localhost port you run the site through
